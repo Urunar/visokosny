a652173b3f1579da1f09f2e77801c2c864c77841
Urunar <139038673+Urunar@users.noreply.github.com>
2025-12-04 21:15:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+def is_leap_year(year):
+    """Определяет, является ли год високосным."""
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
 
+def main():
+    year = int(input("Введите год: "))
+    if is_leap_year(year):
+        print(f"{year} является високосным годом.")
+    else:
+        print(f"{year} не является високосным годом.")
+
+if __name__ == "__main__":
+    main()
