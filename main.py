2259b579fcbfa130d33b3d2b00cd78380b93a27d
Urunar <urunar@gmail.com>
2025-12-05 09:28:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def is_leap_year(year):
+    """comment test git"""
+    """Определяет, является ли год високосным."""
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+def main():
+    while 1 == 1:
+        val = input("Введите год: ")
+        if val == "exit":
+            exit(0)
+        year = int(val)
+        if is_leap_year(year):
+            print(f"{year} является високосным годом.")
+        else:
+            print(f"{year} не является високосным годом.")
+
+if __name__ == "__main__":
+    main()
